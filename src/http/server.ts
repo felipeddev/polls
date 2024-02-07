d020985f7b8662be9bfedcef69c66740cf57acdb
Felipe D. Silva <feliped.dev@proton.me>
2024-02-07 00:01:44 -0300

    feat: :sparkles: add GetPoll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
 import {createPoll} from "./routes/CreatePoll";
+import {getPoll} from "./routes/GetPoll";
 
 const app = fastify();
 
 app.register(createPoll)
+app.register(getPoll)
 app.listen({port: 3000}, (err, address) => {
   if (err) {
     console.error(err);
