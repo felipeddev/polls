e5645a91de36c720f9ff27002e203d3473c261e5
Felipe D. Silva <feliped.dev@proton.me>
2024-02-07 01:11:21 -0300

    feat: :sparkles: add VoteOnPoll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import fastify from "fastify";
 import {createPoll} from "./routes/CreatePoll";
 import {getPoll} from "./routes/GetPoll";
+import {voteOnPoll} from "./routes/VoteOnPoll";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
 
+app.register(cookie, {
+  secret: "generatedSecretKey",
+  hook: 'onRequest'
+});
+
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 app.listen({port: 3000}, (err, address) => {
   if (err) {
     console.error(err);
